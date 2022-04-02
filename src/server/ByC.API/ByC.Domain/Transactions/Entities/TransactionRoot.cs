b1e2b597f84d338b50d549f19ad906448f29b414
Gáyan Justo <gayan.justo@gmail.com>
2022-04-02 12:01:39 -0300

    (chore): adding Transactions context. adding TransactionRoot aggregate adding CNABData model

---
+++ src/server/ByC.API/ByC.Domain/Transactions/Entities/TransactionRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ByC.Domain.Transactions.Entities
+{
+    public class TransactionRoot
+    {
+        public Guid Id { get; private set; }
+        public string Type { get; private set; }
+        public DateTime? Date { get; private set; }
+        public decimal? Value { get; private set; }
+        public string Document { get; private set; }
+        public string Card { get; private set; }
+        public TimeSpan? Hour { get; private set; }
+        public string StoreOwnerName { get; private set; }
+        public string StoreName { get; private set; }
+
+    }
+}
---
+++ src/server/ByC.API/ByC.Domain/Transactions/Models/CNABData.cs
@@ -0,0 +1,18 @@
+﻿namespace ByC.Domain.Transactions.Models
+{
+    public class CNABData
+    {
+        public const int zeroBasedOffset = 1;
+        public string Description { get; private set; }
+        public int IndexStart { get; private set; }
+        public int IndexEnd { get; private set; }
+        public int Length { get; private set; }
+        public CNABData(string description, int indexStart, int indexEnd)
+        {
+            Description = description;
+            IndexStart = indexStart;
+            IndexEnd = indexEnd;
+            Length = (indexEnd - indexStart) + zeroBasedOffset;
+        }
+    }
+}
