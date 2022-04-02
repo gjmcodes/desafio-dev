26be371863fdc00a32037188f6ad1177e7770856
Gáyan Justo <gayan.justo@gmail.com>
2022-04-02 16:56:15 -0300

    (chore): adding tests project

---
+++ src/server/ByC.API/ByC.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ByC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
